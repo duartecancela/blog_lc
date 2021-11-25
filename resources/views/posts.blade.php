4526d2e199c95359a5748cfe7e18739fb70fa3bf
Duarte Cancela <duartecancela@gmail.com>
2021-11-25 18:42:39 +0000

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci in natus quasi repellat totam vel voluptas. Commodi culpa, dolorum enim esse expedita explicabo id illum inventore ipsam iusto libero molestias neque nesciunt obcaecati placeat quibusdam quidem sunt tempora ullam, veritatis? Accusamus ad adipisci assumenda beatae corporis cumque doloremque eligendi esse fugit hic illum inventore ipsam itaque iusto libero magnam minima nemo non porro praesentium, quis quisquam quod reiciendis repellat, repellendus saepe sequi tempora temporibus tenetur, veniam. Assumenda eum ipsa pariatur quas repellat reprehenderit voluptatibus! Aliquid asperiores dolore dolorem eius esse, et facilis hic maiores porro, possimus quasi tempora veritatis?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci in natus quasi repellat totam vel voluptas. Commodi culpa, dolorum enim esse expedita explicabo id illum inventore ipsam iusto libero molestias neque nesciunt obcaecati placeat quibusdam quidem sunt tempora ullam, veritatis? Accusamus ad adipisci assumenda beatae corporis cumque doloremque eligendi esse fugit hic illum inventore ipsam itaque iusto libero magnam minima nemo non porro praesentium, quis quisquam quod reiciendis repellat, repellendus saepe sequi tempora temporibus tenetur, veniam. Assumenda eum ipsa pariatur quas repellat reprehenderit voluptatibus! Aliquid asperiores dolore dolorem eius esse, et facilis hic maiores porro, possimus quasi tempora veritatis?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci in natus quasi repellat totam vel voluptas. Commodi culpa, dolorum enim esse expedita explicabo id illum inventore ipsam iusto libero molestias neque nesciunt obcaecati placeat quibusdam quidem sunt tempora ullam, veritatis? Accusamus ad adipisci assumenda beatae corporis cumque doloremque eligendi esse fugit hic illum inventore ipsam itaque iusto libero magnam minima nemo non porro praesentium, quis quisquam quod reiciendis repellat, repellendus saepe sequi tempora temporibus tenetur, veniam. Assumenda eum ipsa pariatur quas repellat reprehenderit voluptatibus! Aliquid asperiores dolore dolorem eius esse, et facilis hic maiores porro, possimus quasi tempora veritatis?</p>
+    </article>
+</body>
+</html>
